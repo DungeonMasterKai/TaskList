b3a0274d8bb6fc476619ad7d0bb33ce07347c2fe
DungeonMasterKai <zwang529@my.nipissingu.ca>
2023-01-02 17:20:20 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+import sqlite3 as sql
+
+task = [] # Define a empty list
+
+# Function to add task to list
+def add_task():
+    task_string = task_field.get()
+    if len(task_string) == 0:
+        messagebox.showinfo('Error', 'Field is Empty')
+    else:
+        task.append(task_string)
+        the_cursor.execute('insert into tasks values (?)', (task_string,))
+        list_update()
+        task_field.delete(0, 'end')
+
+def list_update():
+    clear_list()
+    for task in tasks:
+        task_listbox.insert('end',task)
+
+def delete_task():
+    try:
+        the_value = task_listbox.get(task_listbox.curselection())
+        if the_value in tasks:
+            tasks.remove(the_value)
+            list_update()
+            the_cursor.execute('delete from tasks where title = ?', (the_value,))
+    except:
+        messagebox.showinfo('Error', 'No Task Selected. Cannot Delete.')
